37ebd5dd0b4efb06434bef4d3c71931bc0660734
maulanazn <startnntnk99@gmail.com>
2024-02-26 19:57:28 +0700

    setting artist edit form to readonly

--- resources/views/artist/edit.blade.php
+++ resources/views/artist/edit.blade.php
@@ -9,16 +9,16 @@
     <form action="{{route('artist_update', $artist->name)}}" method="POST" class="container mt-3 g-3">
         @method('PUT')
         @csrf
-        <input type="text" class="form-control mb-3" name="name" id="name" value="{{$artist->name}}" placeholder="Name...">
+        <input type="text" class="form-control mb-3" name="name" readonly id="name" value="{{$artist->name}}" placeholder="Name...">
         @error('name')
             <div>{{$message}}</div>
         @enderror
 
-        <textarea name="description" id="description" cols="30" rows="10" placeholder="Description...">{{$artist->description}}</textarea>
+        <textarea name="description" id="description" cols="30" readonly rows="10" placeholder="Description...">{{$artist->description}}</textarea>
         @error('description')
             <div>{{$message}}</div>
         @enderror
                 
-        <input type="submit" class="btn btn-primary position-relative start-50 end-0" value="Submit">
+        <!-- <input type="submit" class="btn btn-primary position-relative start-50 end-0" value="Submit"> -->
     </form>
 @endsection
